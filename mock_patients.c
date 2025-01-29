d80692f34bd5caf44ad9881c6ffb26d5e7f8de3b
Deepanshumethi <73273477+Deepanshumethi@users.noreply.github.com>
2025-01-29 18:06:15 +0530

    Updated code

--- mock_patients.c
+++ mock_patients.c
@@ -1,138 +1,97 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-typedef struct patients{
-    int id;
-    char situation[10];
-    struct patients*next;
-    int index;
-}patients;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+typedef struct patients {
+	int id;
+	char situation[10];
+	struct patients *next;
+	int index;
+} patients;
 
-void addEnd(patients** head, int id_input, char* situation_input,int index){
-    patients * newpatient  =malloc(sizeof(patients));
-    newpatient->next = NULL;
-    newpatient->id = id_input;
-    newpatient->index = index;
-    // newpatient->situation = situation_input;
-    strcpy(newpatient->situation,situation_input);
-    if(*head == NULL){
-        *head = newpatient;
-        return;
-    }
-    
-    patients* temp = *head;
-    while(temp->next != NULL){
-        temp = temp->next;
-    }
-    temp->next = newpatient;
-    
-    
+typedef struct map {
+	int id;
+} map;
+
+int checkid(map mapper[],int id,int count) {
+	for(int i=0; i<count; i++) {
+		if(mapper[i].id == id) {
+			return 1;
+		}
+	}
+	return 0;
 }
 
-patients* sortThem(patients* head){
-    patients* store = head;
-    patients* temp = store;
-    patients* result = malloc(sizeof(patients));
-    result->next = NULL;
-    patients* pointer = result;
-    while(temp!= NULL){
-        if(temp->index == 0){
-            printf("Critical\n");
-            pointer->next = temp;
-            
-             pointer = pointer->next;
-            
-        }
-        temp=temp->next;
-    }
-    temp = store;
-    while(temp!= NULL){
-        if(temp->index==1){
-             printf("Serious\n");
-            pointer->next = temp;
-             
-               pointer = pointer->next;
-            
-        }
-        temp=temp->next;
-    }
-    
-    temp = store;
-    while(temp!= NULL){
-        if(temp->index==2){
-             printf("Stable\n");
-            pointer->next = temp;
-            
-            pointer = pointer->next;
-            
-        }
-        temp=temp->next;
-    }
-    // printf("id   %s", result->next->situation);
-    return result;
-    
-    
+void addEnd(patients **head, int id_input, char *situation_input, int index) {
+	patients *newpatient = malloc(sizeof(patients));
+	newpatient->next = NULL;
+	newpatient->id = id_input;
+	newpatient->index = index;
+	strcpy(newpatient->situation, situation_input);
+	if (*head == NULL) {
+		*head = newpatient;
+		return;
+	}
+	patients *temp = *head;
+	while (temp->next != NULL) {
+		temp = temp->next;
+	}
+	temp->next = newpatient;
 }
-
-
-int main(){
-    
-    int number;
-    scanf("%d",&number);
-      patients* head = NULL;
-    
-    // patients *list = malloc(number*sizeof(number));
-    for(int i=0;i<number;i++){
-        int id_input;
-        scanf("%d",&id_input);
-        // list[i].id = id_input;
-        getchar();
-        char* situation_input;
-        scanf("%s",situation_input);
-        // strcpy(list[i].situation,situation_input);
-        if(strcmp(situation_input,"Critical")==0){
-            // printf("true");
-             addEnd(&head,id_input,situation_input,0);
-            continue;
-        }
-        if(strcmp(situation_input,"Serious")==0){
-            // printf("true");
-             addEnd(&head,id_input,situation_input,1);
-            continue;
-        }
-       if(strcmp(situation_input,"Stable")==0){
-            // printf("true");
-             addEnd(&head,id_input,situation_input,2);
-            continue;
-        }
+int takeValid(char* str){
+    if(strcmp(str,"critical") ==0 || strcmp(str,"stable")==0 || strcmp(str,"serious") == 0){
+        return 1;
     }
-    
-  
-  
-  
-//   while(temp != NULL){
-//       printf("%d %s\n",temp->id,temp->situation);
-//       temp=temp->next;
-//   }
-  
-  
-  head = sortThem(head);
-//   printf("%s",head->situation);
-    patients* temp = head->next;
-    //  temp = head;
-  int count = 1;
-  while(temp != NULL){
-      
-      printf("%d  %d %s\n",count, temp->id,temp->situation);
-      count++;
-      temp=temp->next;
-  }
-    
-    
-
-    
-    
-    
     return 0;
+}
+
+int main() {
+	int number;
+	printf("Enter number of entries:- ");
+	scanf("%d", &number);
+	map mapper[number];
+	int map_count = 0;
+	patients *critical = NULL;
+	patients *serious = NULL;
+	patients *stable = NULL;
+
+	while (number>0) {
+		int id_input;
+		scanf("%d", &id_input);
+		getchar();
+		char situation_input[10];
+		scanf("%s", situation_input);
+		while(takeValid(situation_input) == 0){
+		    printf("Enter the valid situation name(serious , critical , stable):- ");
+		    scanf("%s",situation_input);
+		}
+		if(checkid(mapper,id_input,map_count) == 0) {
+			mapper[map_count++].id = id_input;
+			number--;
+			if (strcmp(situation_input, "critical") == 0) {
+				addEnd(&critical, id_input, situation_input, 0);
+			} else if (strcmp(situation_input, "serious") == 0) {
+				addEnd(&serious, id_input, situation_input, 1);
+			} else if (strcmp(situation_input, "stable") == 0) {
+				addEnd(&stable, id_input, situation_input, 2);
+			}
+		} else {
+			printf("Enter a unique id:-");
+		}
+	}
+	patients *temp = critical;
+	while (temp->next != NULL) {
+		temp = temp->next;
+	}
+	temp->next = serious;
+	while (temp->next != NULL) {
+		temp = temp->next;
+	}
+	temp->next=stable;
+	printf("\n\n");
+	while(critical!= NULL) {
+		printf(" %d %s\n", critical->id, critical->situation);
+		critical=critical->next;
+	}
+	return 0;
 }
